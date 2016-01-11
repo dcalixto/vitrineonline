c2935ec5d553961b35f75c3a37e52b88f66ce15e
mdn <dm.zgroupsoft@gmail.com>
2016-01-11 11:46:39 +0300

    fixed usage of dynamic_selectable gem

--- app/controllers/dynamic_selectable/gender_categories_controller.rb
+++ app/controllers/dynamic_selectable/gender_categories_controller.rb
@@ -1,7 +1,7 @@
 module DynamicSelectable
   class GenderCategoriesController < SelectController
     def index
-      categories = Category.where(gender_id: params[:gender_id]).select('id, gender').order('gender asc')
+      categories = Category.where(gender_id: params[:gender_id]).select('id, name').order('name asc')
       render json: categories
     end
   end
