41fd00b52443764d0b79bdd2c7f7df743a97abcc
Daniel Calixto <calixtodaniel@gmail.com>
2016-03-02 17:41:55 -0300

    fixing carrierwave fog not upload to s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,6 @@ Vitrineonline::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # config.action_mailer.default_url_options = { :host => 'shielded-eyrie-5443.herokuapp.com' }
-
-#config.action_controller.asset_host = 'http://vitrineonline.s3.amazonaws.com'
 
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,49 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAJW75RJ325TZWF5JA',
+    aws_secret_access_key: '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',
+    region: 'sa-east-1'
+  }
 
-    config.storage = :fog
-
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIAJW75RJ325TZWF5JA',
-      aws_secret_access_key: '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',
-      region: 'sa-east-1'
-    }
-    config.fog_directory  = 'vitrineonline'
-    config.fog_public     = true
-
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.development?
+    config.storage           = :file
+    config.enable_processing = false
+    config.root              = "#{Rails.root}/tmp"
   else
-    # for development and testing locally
-    config.storage = :file
-    config.enable_processing = true
+    config.storage = :fog
+  end
 
+  config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
+  config.fog_directory  = 'vitrineonline'
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
 end
-end
+
+
+
+
+
+#CarrierWave.configure do |config|
+  #if Rails.env.production?
+
+  #  config.storage = :fog
+
+  #  config.fog_credentials = {
+  #    provider: 'AWS',
+  #    aws_access_key_id: 'AKIAJW75RJ325TZWF5JA',
+  #    aws_secret_access_key: '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',
+  #    region: 'sa-east-1'
+  #  }
+  #  config.fog_directory  = 'vitrineonline'
+  #  config.fog_public     = true
+  #  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+
+#  else
+    # for development and testing locally
+    #config.storage = :file
+  #  config.enable_processing = true
+
+#end
+#end
