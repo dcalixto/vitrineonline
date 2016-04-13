b4170c0192dbe16bc4df810a704acb6251aa6301
Daniel Calixto <calixtodaniel@gmail.com>
2016-04-13 15:07:22 -0300

    removed premailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@
 class UserMailer < ActionMailer::Base
   default from: 'Vitrineonline'
 
-  def premailer(message)
-    message.text_part.body = Premailer.new(message.text_part.body.to_s, with_html_string: true).to_plain_text
-    message.html_part.body = Premailer.new(message.html_part.body.to_s, with_html_string: true).to_inline_css
+  #def premailer(message)
+  #  message.text_part.body = Premailer.new(message.text_part.body.to_s, with_html_string: true).to_plain_text
+  #  message.html_part.body = Premailer.new(message.html_part.body.to_s, with_html_string: true).to_inline_css
 
-    return message
-  end
+  #  return message
+  #end
 
   def registration_confirmation(user)
      @user = user
