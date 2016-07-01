195daf40fe603ad2f6d4433684726a22b89a7623
Daniel Calixto <calixtodaniel@gmail.com>
2016-07-01 13:02:56 -0300

    added foreman tasks to deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -82,8 +82,70 @@ task setup: :environment do
 end
 
 
-desc 'Export foreman'
-  cmd = "rbenv sudo foreman export upstart /etc/init -a #{foreman_app} -u #{foreman_user} -l #{foreman_log}"
+
+
+
+
+
+set_default :foreman_app,  lambda { application }
+set_default :foreman_user, lambda { user }
+set_default :foreman_log,  lambda { "#{deploy_to!}/#{shared_path}/log" }
+
+namespace :foreman do
+  desc 'Export the Procfile to Ubuntu upstart scripts'
+  task :export do
+    export_cmd = "rbenv sudo foreman export upstart /etc/init -a #{foreman_app} -u #{foreman_user} -l #{foreman_log}"
+
+    queue %{
+      echo "-----> Exporting foreman procfile for #{foreman_app}"
+      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{export_cmd}]}
+    }
+  end
+
+  desc "Start the application services"
+  task :start do
+    queue %{
+      echo "-----> Starting #{foreman_app} services"
+      #{echo_cmd %[sudo start #{foreman_app}]}
+    }
+  end
+
+  desc "Stop the application services"
+  task :stop do
+    queue %{
+      echo "-----> Stopping #{foreman_app} services"
+      #{echo_cmd %[sudo stop #{foreman_app}]}
+    }
+  end
+
+  desc "Restart the application services"
+  task :restart do
+    queue %{
+      echo "-----> Restarting #{foreman_app} services"
+      #{echo_cmd %[sudo start #{foreman_app} || sudo restart #{foreman_app}]}
+    }
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 desc 'Deploys the current version to the server.'
