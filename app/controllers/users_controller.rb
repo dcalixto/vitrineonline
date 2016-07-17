a5b3aa17a2bf1acf714c0021cd3da6b7eee44b9b
Daniel Calixto <calixtodaniel@gmail.com>
2016-07-16 22:42:47 -0300

    added user object again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
     @vitrine = Vitrine.new(params[:vitrine])
 
     if @user.save
-      UserMailer.registration_confirmation(:user_id).deliver
+      UserMailer.registration_confirmation(@user).deliver
       redirect_to root_url
 
       flash[:success] = "Por favor confirme seu endereço de email para continuar".html_safe
