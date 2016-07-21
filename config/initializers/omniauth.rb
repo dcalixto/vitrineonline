099bc2bf2f4d2e135a16823b5f42063d5ae359ac
Den M <dm.zgroupsoft@gmail.com>
2016-07-21 22:14:59 +0300

    Fixed missing name for facebook signup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@ ENV['TWITTER_API_SECRET'] = 'TWITTER_CONSUMER_API_SECRET'
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: "email, publish_actions", provider_ignores_state: true
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: "email, publish_actions", info_fields: 'email, first_name, last_name', provider_ignores_state: true
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 end
