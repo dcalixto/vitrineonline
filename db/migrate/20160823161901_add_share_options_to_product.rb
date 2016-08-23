60d660056b8740d8a8f5d8df801e995065ec1292
Daniel Calixto <calixtodaniel@gmail.com>
2016-08-23 13:28:28 -0300

    added share on facebook migration mistekly deleted

---
+++ db/migrate/20160823161901_add_share_options_to_product.rb
@@ -0,0 +1,8 @@
+class AddShareOptionsToProduct < ActiveRecord::Migration
+  def change
+  
+  add_column :products, :is_shared_on_facebook, :boolean, default: false
+  add_column :products, :is_shared_on_twitter, :boolean, default: false
+  
+  end
+end
