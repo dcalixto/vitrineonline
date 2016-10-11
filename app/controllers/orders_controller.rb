7375bcbd4a9a4ebf6cefcc05bd13e813b5c0adb0
Daniel Calixto <calixtodaniel@gmail.com>
2016-10-10 22:13:41 -0300

    removed paypal config file

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -57,7 +57,13 @@ class OrdersController < ApplicationController
 
 
 
-
+require 'paypal-sdk-adaptivepayments'
+PayPal::SDK.configure(
+  :mode      => "live",  # Set "live" for production
+  :app_id    => "Abkh6ZginWQ3UPu22C0JNT5lh6nivSJdgi83dB4UULnxTNxA4lFlCsHUwBtBo6ywimJtwApythzi79Le",
+  :username  => "calixtodaniel_api1.gmail.com",
+  :password  => "DSG2N7ZTJELQUNWL",
+  :signature => "ApTm-PAyPlvJvccJvw3u97zEAvNNAKnr2ZdPlbdLvJvW9XEWP2Rr3Csv" )
 
 
 @api = PayPal::SDK::AdaptivePayments.new
@@ -134,7 +140,6 @@ class OrdersController < ApplicationController
 
 
 @response = @api.pay(@pay)
-#@pay_response = @api.pay(@pay)
 
 # Access response
 if @response.success? && @response.payment_exec_status != "ERROR"
