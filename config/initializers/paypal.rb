3dd8f4ca3b9a4dea22b0505a6946bb1dcd3fefe8
Daniel Calixto <calixtodaniel@gmail.com>
2016-10-25 17:26:50 -0200

    get back paypal yml

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,11 +1,11 @@
-#PayPal::SDK.load("config/paypal.yml", Rails.env)
+PayPal::SDK.load("config/paypal.yml", Rails.env)
+PayPal::SDK.logger = Rails.logger
 
-require 'paypal-sdk-adaptivepayments'
- PayPal::SDK.logger = Rails.logger
-  PayPal::SDK.configure(
-     :mode      => "live",  # Set "live" for production
-     :app_id    => "APP-8TU98166249274123",
-     :username  => "admin_api1.vitrineonline.com",
-     :password  => "DKJVG8KMXTBFWZFT",
-     :signature => "AFcWxV21C7fd0v3bYYYRCpSSRl31AWU78If4EWNK1xJLuqvuBIF7s3dY" )
+#require 'paypal-sdk-adaptivepayments'
+  # PayPal::SDK.configure(
+ #    :mode      => "live",  # Set "live" for production
+   #  :app_id    => "APP-8TU98166249274123",
+   #  :username  => "admin_api1.vitrineonline.com",
+   #  :password  => "DKJVG8KMXTBFWZFT",
+   #  :signature => "AFcWxV21C7fd0v3bYYYRCpSSRl31AWU78If4EWNK1xJLuqvuBIF7s3dY" )
 
