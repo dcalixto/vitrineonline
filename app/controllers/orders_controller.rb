a42fde178842ee386692c68fe14b3b8c2c3ef129
Daniel Calixto <calixtodaniel@gmail.com>
2016-11-07 22:06:48 -0200

    test paypal sdk without yml

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -51,19 +51,7 @@ class OrdersController < ApplicationController
 
   def buy
 
-    require 'paypal-sdk-adaptivepayments'
-
-
-   
-
-    PayPal::SDK.configure(
-     :mode      => "live",  # Set "live" for production
-     :app_id    => "APP-8TU98166249274123",
-     :username  => "admin_api1.vitrineonline.com",
-     :password  => "DKJVG8KMXTBFWZFT",
-     :signature => "AFcWxV21C7fd0v3bYYYRCpSSRl31AWU78If4EWNK1xJLuqvuBIF7s3dY" )
-
- @api = PayPal::SDK::AdaptivePayments.new
+   @api = PayPal::SDK::AdaptivePayments.new
 
 
 
