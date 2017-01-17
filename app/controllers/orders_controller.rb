bf854b2b1a1d6ade58316e0d9b81ac7db65a07b9
Daniel Calixto <calixtodaniel@gmail.com>
2017-01-17 18:10:30 -0200

    test ipn

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -107,9 +107,9 @@ order = Order.find(params[:id])
 
   def ipn_notification
   #  byebug
-    ipn = PayPal::SDK::Core::API::IPN.new
+   # ipn = PayPal::SDK::Core::API::IPN.new
     
-    ipn.send_back(request.raw_post)
+   # ipn.send_back(request.raw_post)
 
 
  if PayPal::SDK::Core::API::IPN.valid?(request.raw_post)
