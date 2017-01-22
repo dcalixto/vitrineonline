34f78855131914dce9bac2671a29674e50d0f41b
Daniel Calixto <calixtodaniel@gmail.com>
2017-01-22 04:47:14 -0200

    removed avatar to banner js, removed token input and added ifoto to similarities

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,11 +28,11 @@
 
 //= require jquery.liquidcarousel
 //= pagination
-//= jquery.infinitescroll
+
 //= require_tree .
 
 
-//require jquery.tokeninput
+
 
 $(document).ready(function() {
 NProgress.configure({ showSpinner: false });
@@ -204,16 +204,7 @@ $(document).ready(function(){
 	// disable auto discover
 	Dropzone.autoDiscover = false;
 
-  $("#user_avatar_dropzone").dropzone({
-    maxFilesize: 2, // Set the maximum file size to 256 MB
-    maxFiles: 1,
-    dictDefaultMessage: "Solte suas imagens aqui",
-	paramName: "user[avatar]", // Rails expects the file upload to be something like model[field_name]
-	addRemoveLinks: true, // Don't show remove links on dropzone itself.
-    dictRemoveFile: 'Remover',
-    acceptedFiles: "image/*"
-  });
-
+ 
 	//dropzone.on("success", function(file) {
 		//this.removeFile(file)
 		//$.getScript("/edit")
@@ -307,15 +298,7 @@ $(function() {
 
 // TOKEN INPUT TAGS
 
-$(function() {
-  $("#product_tags").tokenInput("/products/tags.json", {
-    prePopulate: $("#product_tags").data("pre"),
-    preventDuplicates: true,
-    theme: 'mac',
-    noResultsText: "No results, needs to be created.",
-    animateDropdown: false
-  });
-});
+
 
 
 // MARKDOWN TEXT
--- app/assets/javascripts/dropzone_banner.js
+++ app/assets/javascripts/dropzone_banner.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
-  	Dropzone.autoDiscover = false;
-$('#banner').dropzone({// PDF dropzone element
-  maxFilesize: 2, // Set the maximum file size to 256 MB
-   maxFiles: 1,
-    dictDefaultMessage: "Solte suas imagens aqui",
-  paramName: "marketing[banner]", // Rails expects the file upload to be something like model[field_name]
-  addRemoveLinks: true, // Don't show remove links on dropzone itself.
- dictRemoveFile: 'Remover'
-    // rest of code
-});
+$(document).ready(function(){
+	// disable auto discover
+	Dropzone.autoDiscover = false;
 
-});
+  $("#user_avatar_dropzone").dropzone({
+    maxFilesize: 2, // Set the maximum file size to 256 MB
+    maxFiles: 1,
+    dictDefaultMessage: "Solte suas imagens aqui",
+	paramName: "user[avatar]", // Rails expects the file upload to be something like model[field_name]
+	addRemoveLinks: true, // Don't show remove links on dropzone itself.
+    dictRemoveFile: 'Remover',
+    acceptedFiles: "image/*"
+  });
+  });
