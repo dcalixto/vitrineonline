a332bb8cf752fee304d5401380e3aa44f6155aa0
Daniel Calixto <calixtodaniel@gmail.com>
2017-01-27 23:13:38 -0200

    fix star thing and test cart if vender delete account

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,4 +1,4 @@
 #PayPal::SDK.load("config/paypal.yml", Rails.env)
-#PayPal::SDK.load('config/paypal.yml',  ENV['RACK_ENV'] || 'development')
-PayPal::SDK.load('config/paypal.yml',  ENV['RACK_ENV'] || 'production')
+PayPal::SDK.load('config/paypal.yml',  ENV['RACK_ENV'] || 'development')
+#PayPal::SDK.load('config/paypal.yml',  ENV['RACK_ENV'] || 'production')
 PayPal::SDK.logger = Rails.logger
