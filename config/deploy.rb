953165f45e4c429af979e6917f88035fa71b595d
Daniel Calixto <calixtodaniel@gmail.com>
2017-01-28 02:37:06 -0200

    test mina stack

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,78 +1,38 @@
+require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
- require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (https://rvm.io)
-require "config/recipes/monit"
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
+require 'mina/rbenv'
+require 'mina-stack'
 
-set :domain, '52.87.228.48'
-set :deploy_to, '/home/ubuntu/vitrineonline'
-set :user, 'ubuntu' # Optional
-set :identity_file, '/Users/danielcalixto/gsg-keypair'
-set :repository, 'git@github.com:dcalixto/vitrineonline.git'
+set :app,                 'vitrineonline'
+set :server_name,         'vitrineonline.com'
+set :keep_releases,       1
+set :default_server,      :production
 set :branch, 'master'
+set :identity_file, '/Users/danielcalixto/gsg-keypair'
+
 set :forward_agent, true
 set :term_mode, nil
 set :port, '22'
-set :rails_env, 'production'
-set :repository_name, 'vitrineonline'
-set :rbenv_path, '$HOME/.rbenv'# Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
-#   set :port, '30000'           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+set :rbenv_path, '$HOME/.rbenv'
 
-# shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# set :shared_dirs, fetch(:shared_dirs, []).push('log')
+
+
+set :server, ENV['to'] || default_server
+invoke :"env:#{server}"
+
+# Allow calling as `mina deploy at=master`
+set :branch, ENV['at']  if ENV['at']
+
+
+
  set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
 
 
 
-set :shared_paths, %w[
-  public/uploads
-  
-]
-
-
-
-set :monitored,                     %w(
-                                      nginx
-                                      postgresql
-                                      redis
-                                      private_pub
-                                      memcached
-                                    )
-
-
-
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
-task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-   invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
-
-  queue! %[mkdir -p "#{deploy_to}/shared/public/uploads"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/public/uploads"]
-end
-
-# Put any custom commands you need to run at setup
-# All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
   # command %{rbenv install 2.3.0}
-
-
-
-
-
-
 
  in_path(fetch(:shared_path)) do
 
@@ -99,42 +59,61 @@ task :setup do
     command %[chmod -R o-rwx config]
 
     
-    command %[mkdir public/uploads]
-
+   
   end
-
-
-
 
 
 end
 
+
+
+
+
+
+
+
+
+set :server_stack,                  %w(
+                                      monit
+                                                                         
+                                    )
+
+set :shared_paths,                  %w(
+                                      tmp
+                                      log
+                                      config/database.yml
+                                      config/application.yml
+                                      public/uploads
+                                    )
+
+set :monitored,                     %w(
+                                      nginx
+                                      postgresql
+                                      redis
+                                      private_pub
+                                      memcached
+                                    )
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
 desc "Deploys the current version to the server."
 task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
 
-    on :launch do
-      in_path(fetch(:current_path)) do
+    to :launch do
+     # invoke :'private_pub:restart'
+       in_path(fetch(:current_path)) do
         command %{mkdir -p tmp/}
         command %{touch tmp/restart.txt}
       end
+
     end
   end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run(:local){ say 'done' }
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - https://github.com/mina-deploy/mina/tree/master/docs
