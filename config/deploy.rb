e9ef625400e98eb89ebec394ce0d2868d9ad008c
Daniel Calixto <calixtodaniel@gmail.com>
2017-02-02 08:21:50 -0200

    fix rbenv4

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,27 +71,6 @@ end
 
 
 
-set :server_stack,                  %w(
-                                      monit
-
-)
-
-set :shared_paths,                  %w(
-                                      tmp
-                                      log
-                                      config/database.yml
-                                      config/application.yml
-                                      public/uploads
-)
-
-set :monitored,                     %w(
-                                      nginx
-                                      postgresql
-                                      redis
-                                      private_pub
-                                      memcached
-                                    
-)
 
 desc "Deploys the current version to the server."
 task :deploy do
