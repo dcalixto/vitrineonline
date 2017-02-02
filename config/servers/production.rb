781aa613be2d839dacda8c804488140a38632b13
Daniel Calixto <calixtodaniel@gmail.com>
2017-02-02 07:37:55 -0200

    test deploy mina without server config

--- config/servers/production.rb
+++ config/servers/production.rb
@@ -1,16 +1,16 @@
-namespace :env do
-  task :production => [:environment] do
-    set :domain,              '52.87.228.48'
-    set :user,                'ubuntu'
-    set :deploy_to,           '/home/ubuntu/vitrineonline'
-    set :repository,           'git@github.com:dcalixto/vitrineonline.git'
-    set :nginx_path,          '/etc/nginx'
-    set :deploy_server,       'production'                   # just a handy name of the server
-    set :rails_env,           'production'
-    set :branch,              'master'
-    invoke :defaults                                         # load rest of the config
-  end
-end
+#namespace :env do
+#  task :production => [:environment] do
+#    set :domain,              '52.87.228.48'
+#    set :user,                'ubuntu'
+#    set :deploy_to,           '/home/ubuntu/vitrineonline'
+#    set :repository,           'git@github.com:dcalixto/vitrineonline.git'
+#    set :nginx_path,          '/etc/nginx'
+ #   set :deploy_server,       'production'                   # just a handy name of the server
+ #   set :rails_env,           'production'
+ #   set :branch,              'master'
+#    invoke :defaults                                         # load rest of the config
+#  end
+#end
 
 
 
