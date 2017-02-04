d9778152a561e6796f9312a7b7414f3a85b3bd82
Daniel Calixto <calixtodaniel@gmail.com>
2017-02-04 20:06:36 -0200

    test welcome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,8 +54,10 @@ class UsersController < ApplicationController
       @user.update_attribute(:login_at, Time.zone.now)
       @user.update_attribute(:ip_address, request.remote_ip)
       cookies[:auth_token] = {:value => @user.auth_token, :expires => 3.month.from_now}
+        redirect_to  page_path('welcome')
+
       redirect_to root_url
-      flash[:success] = "Bem vindo a Vitrineonline #{(@user.first_name)}".html_safe
+     # flash[:success] = "Bem vindo a Vitrineonline #{(@user.first_name)}".html_safe
     else
       render :new
       flash[:error] = 'Ooooppss, algo deu errado!'.html_safe
