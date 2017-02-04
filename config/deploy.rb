8e816219544e700735b6fcf4eceb2da6382852d4
Daniel Calixto <calixtodaniel@gmail.com>
2017-02-04 08:17:22 -0200

    removed bower6

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,7 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'bower:install_assets'
+ 
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
