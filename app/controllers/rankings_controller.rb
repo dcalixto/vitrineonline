c9e9f253b9cb8940313c1b1d0736f1ac79048c91
Daniel Calixto <calixtodaniel@gmail.com>
2017-02-09 12:00:51 -0200

    fix message and vote ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,6 +1,6 @@
 class RankingsController < ApplicationController
   def index
     @products = Product.order(:cached_weighted_average => :desc)
-    @vitrines = Vitrine.get_likes(:vote_scope => 'rank').size
+    @vitrines = Vitrine.all
   end
 end
