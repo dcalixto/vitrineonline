179feec4553977df03b222f5d57b05b8bcf2ff20
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-06 01:14:00 -0300

    test order mailerd

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -8,13 +8,13 @@ class OrderMailer < ActionMailer::Base
 
 
   def order_confirmation(order)
-    order_identification = order.order_id 
+    @order = order
     @user = order.buyer_id
     mail(to: @user.email, subject: 'Confirmação da Compra', &:html)
   end
 
  def order_confirmation_seller(order)
-    order_identification = order.order_id 
+     @order = order
     @vitrine = order.seller_id
     mail(to: @seller.email, subject: 'Confirmação da Venda', &:html)
   end
