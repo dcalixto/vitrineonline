a0c88a606f5990befb191dbf7d55d9865b2f462d
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-05 22:26:17 -0300

    test fnd order on order mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -8,14 +8,14 @@ class OrderMailer < ActionMailer::Base
 
 
   def order_confirmation(order)
-    @order = order
-    @buyer = order.buyer
+    order = Order.find(order_id)
+    @buyer = order.buyer_id
     mail(to: @buyer.email, subject: 'Confirmação da Compra', &:html)
   end
 
  def order_confirmation_seller(order)
-    @order = order
-    @seller = order.seller
+    order = Order.find(order_id)
+    @seller = order.seller_id
     mail(to: @seller.email, subject: 'Confirmação da Venda', &:html)
   end
 
