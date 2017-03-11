09a0c6110f09b11c73fe6ee22bc571f4f37f3b3c
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-10 22:08:39 -0300

    passing params on producthsdfddff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -151,7 +151,7 @@ class ProductsController < ApplicationController
     @product = current_vitrine.products.build(params[:product])
     if @product.save
       
-    redirect_to product_step_path(@product.id, @product.form_steps.first, only_path: true, format: :html)
+    redirect_to product_step_path(@product.id, @product.form_steps.first, :id => "first", :product_id => @product.id, only_path: true, format: :html)
     else
       render :new, format: :html
     end
