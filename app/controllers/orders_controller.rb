7375b84ecda7c1943c3fca050a5ea06d690fe98f
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-12 03:51:37 -0300

    test order soldjaaaaaa

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,10 +16,7 @@ class OrdersController < ApplicationController
     end
   end
   def sold
-  #   if current_vitrine
-#  @vitrine = current_user.vitrine
-#@order =  @vitrine.orders.find(params[:id])
-@order = Order.find_by_id(params[:id])
+ order = Order.find(params[:id])
 @q = Order.where('seller_id = ? and status = ?', current_vitrine.id, params[:status] || Order.statuses[0]).ransack(params[:q])
     @orders = @q.result(distinct: true).paginate(page: params[:page], per_page: 22)
    
