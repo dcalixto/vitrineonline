367699c77b08547886c9cb610c9ea4b6bc7bcf28
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-18 15:46:54 -0300

    show me the moneydaszgaaaaaaaa6dda

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,9 +3,9 @@ class TransactionsController < ApplicationController
 def show
 order = Order
   
-@current_buyer = buyer.id
+
   
-  @transaction = current_buyer.orders.transaction.find(params[:id]) 
+  @transaction = current_user.orders.transaction.find(params[:id]) 
 
 end
 
