0b54ca7a84d4c5e740c36dafa7a88a27a0553867
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-19 00:26:18 -0300

    added user id to transactionssssas

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,11 +3,12 @@ class TransactionsController < ApplicationController
 def show
 
   
-user = transaction.user_id
-
-@transaction = user.transactions.find(params[:id])
+if @transaction.user_id = @transaction.current_user.id 
+@transaction = Transaction.find(params[:id])
   
-
+else
+   redirect_to :back   
+   flash[:error] = 'Política Atualizada'
 end
-
+end
 end
