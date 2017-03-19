fc85ad77e54fb5ea781df6571eee05a623c8c885
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-19 00:15:01 -0300

    added user id to transactionssssas

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,13 +3,11 @@ class TransactionsController < ApplicationController
 def show
 
   
-#@current_user = current_user
-  #@transaction = current_user.transactions.find(params[:id])
+user = transaction.user_id
+
+@transaction = user.transactions.find(params[:id])
   
 
- @transaction =  Transaction.where(user_id: user.id, order_id: order.id)
 end
-
-
 
 end
