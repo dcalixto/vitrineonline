2727d2c0245fc8e0189f83f1005c155ad1a4812b
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-21 00:35:45 -0300

    removed transaction from transactionsssa135111111

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -196,7 +196,7 @@ class OrdersController < ApplicationController
           order.decrease_products_count
           transaction = Transaction.new
           transaction.store_fee = order.store_fee
-         # transaction.user_id = order.buyer_id
+          transaction.user_id = order.buyer_id
 
           transaction.transaction_id = params[:transaction]['0']['.id_for_sender_txn']
           transaction.status = params[:status]
