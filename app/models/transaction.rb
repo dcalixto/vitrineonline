9646e6ebc85ea9bfedd3e1d83437ed37981de4bb
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-26 06:04:53 -0300

    feedbackships create on feedbacks create1q

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,9 @@
 class Transaction < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :order # , :touch => true
+  belongs_to :user # , :touch => true
 
-  #belongs_to :user # , :touch => true
-
-
-  attr_accessible :store_fee, :transaction_id, :status
+  attr_accessible :store_fee, :transaction_id, :status,:order_id,:user_id
 
   scope :stats, ->(date_range) { select('DATE(transactions.created_at) as day, count(*) as count').group('DATE(transactions.created_at)').where(created_at: date_range) }
   scope :products_stats, ->(date_range) { select('p.id as product_id, count(*) as tcount').joins(:order).joins('INNER JOIN product_data p on(p.id = orders.product_id)').where(created_at: date_range).group('p.id').order('tcount desc') }
