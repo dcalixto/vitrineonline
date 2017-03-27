85b37991305d4af5ca08d353605469f57d96a998
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-27 06:22:18 -0300

    test feedbackshipq111

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -201,14 +201,17 @@ class OrdersController < ApplicationController
           transaction.status = params[:status]
           order.transaction = transaction
 
-           feedbackship = Feedbackship.new
+         
+          if order.save
+
+            feedbackship = Feedbackship.new
             feedbackship.product_id = order.product_id
              feedbackship.feedback_id = order.feedback_id
             feedbackship.order_id = order.id
-
-          order.save
+            feedbackship.save
           OrderMailer.order_confirmation(order).deliver
- 
+          end
+          
         end
       end
 
