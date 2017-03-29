ae4064dff59eddbfefa83df3223a80f96752b800
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-29 05:30:55 -0300

    prdosy1aa1qq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,24 +10,6 @@ class HomeController < ApplicationController
  def index
     @vitrines = Vitrine.all
 
-prodbacks = Prodback.scoped
-
-#@total_feedbacks = Product.joins.(:orders).where('orders = ?',@orders).where('buyer_feedback_date is not null').count
-#from_nil = Picture.where(imageable: nil).where_values.reduce(:and)
-
-
-  # @total_feedbacks = Product.joins(:feedbacks).where('feedbacks = ?', @feedbacks).where('buyer_feedback_date is not null').count
-
-
-#@average_rating = Product.joins(:feedbacks).where('feedbacks = ?', @feedbacks).where('buyer_feedback_date is not null').rated(Feedback::FROM_BUYERS).average(:buyer_rating)
-
-
- # @total_feedbacks = Feedback.by_participant(@products.feedbacks, Feedback::FROM_BUYERS).count
-     
- #   @average_rating = Feedback.average_rating(@vitrine.user.products, Feedback::FROM_BUYERS)
-  
-#feedbacks = Feedback.all
-
  
     @orders = Order.includes(:products)
 
@@ -36,71 +18,25 @@ prodbacks = Prodback.scoped
     @suggestions = Product.unscoped.for_ids_with_order(ids)
 
     if params[:tag]
-   #   @average_customer_rating = Product.where('buyer_feedback_date is not null').rated(Product::FROM_BUYERS).average(:buyer_rating) || 0
-
-      
-      #  @average_rating  = Product.prodbacks.average_rating
-      
-      
-     # @total_feedbacks  = Product.where('buyer_feedback_date is not null').count
-# @total_feedbacks = Product.prodbacks.by_participant.count
-
-  # @total_feedbacks = Product.includes(:prodbacks)#.by_participant.count
-
-#@total_feedbacks = Product.prodbacks.where('buyer_feedback_date is not null').count
-
-
-#@average_rating  = Product.prodbacks.where('buyer_feedback_date is not null').rated.average(:buyer_rating) || 0
-
-
- @average_rating  = Product.includes(:prodbacks)#.where('buyer_feedback_date is not null').rated.average(:buyer_rating) || 0
-
-@total_feedbacks =  Product.includes(:prodbacks)#.where('buyer_feedback_date is not null').count
-
-
-
-
-
-
-
-
-# @total_feedbacks = Prodback.by_participant.count
- #   @average_rating = Prodback.average_rating
-     
- #   @average_rating = Feedback.average_rating(@vitrine.user.products, Feedback::FROM_BUYERS)
-
 
 
       @products = Product.includes(:images,:vitrine).tagged_with(params[:tag]).order('DESC').limit(22)
+  
+    
+    
+    
+    
+      @total_feedbacks = Proback.joins(:product).where('products.id = ?', @product.id).where('buyer_feedback_date is not null').count
+    @average_rating = Proback.joins(:product).where('products.id = ?', @product.id).where('buyer_feedback_date is not null').rated.average(:buyer_rating)
+
+
+    
+    
     else
   
       @products = Product.includes(:images,:vitrine).all
    
-     # @average_customer_rating = Product.where('buyer_feedback_date is not null').rated(Product::FROM_BUYERS).average(:buyer_rating) || 0
-# @average_rating  = Product.prodbacks.average_rating#(FROM_BUYERS)
 
-
-      
-    #  @average_customer_rating = feedbacks.where('buyer_feedback_date is not null').rated(Feedback::FROM_BUYERS).average(:buyer_rating) || 0
-# @total_feedbacks  = Product.where('buyer_feedback_date is not null').count
-
- # @total_feedbacks = Product.prodbacks.by_participant.count
-
-
-#@total_feedbacks = Product.prodbacks.where('buyer_feedback_date is not null').count
-
-
- #   @average_rating  = Product.prodbacks.where('buyer_feedback_date is not null').rated.average(:buyer_rating) || 0
-
-# @total_feedbacks = Product.includes(:prodbacks)
- # @average_rating  = Product.includes(:prodbacks)
-
-
-  
- @average_rating  = Product.includes(:prodbacks)#.where('buyer_feedback_date is not null').rated.average(:buyer_rating) || 0
-
-@total_feedbacks =  Product.includes(:prodbacks)#.where('buyer_feedback_date is not null').count
- 
 
   
   end
