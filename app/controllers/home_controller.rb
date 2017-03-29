bed9bba0c1e43414658e871657624caec6c31061
Daniel Calixto <calixtodaniel@gmail.com>
2017-03-29 17:48:23 -0300

    prdosy1aa1qqqaq1q11111121

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,7 +34,7 @@ class HomeController < ApplicationController
 products_ids = @products.collect(&:id)
 @average_rating  = 
   Prodback.where(product_id: products_ids).
-    where('buyer_feedback_date  not null').rated.
+    where('buyer_feedback_date is not null').rated.
    # group(:product_id).
     average(:buyer_rating)
     
