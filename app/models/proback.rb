e354a23b3c6d81209113d2bb164ffb33279b48af
Daniel Calixto <calixtodaniel@gmail.com>
2017-04-03 20:48:37 -0300

    test probacka

--- app/models/proback.rb
+++ app/models/proback.rb
@@ -12,34 +12,7 @@ belongs_to :product
 
 
 
-  FROM_BUYERS = 'from_buyers'
-  FROM_SELLERS = 'from_sellers'
-
-  NOT_RATED = 0
-
-
-  
-  scope :by_participant, lambda { |user, from_who|
-    case from_who
-    when FROM_BUYERS
-      where('user_id = ? and buyer_feedback_date is not null', user ? user.id : 0).order('buyer_feedback_date desc')
-         end
-  }
-
-  scope :rated, ->(from_who) { where("#{from_who == Feedback::FROM_BUYERS ? 'buyer_rating' : 'seller_rating'} <> ?", Feedback::NOT_RATED) }
-
-  scope :from_buyers_for_product, ->(product_id) { joins(:product).where(products: { id: product_id }).where.not(buyer_feedback_date: nil) }
-
-
-  def self.average_rating(user, from_who)
-    case from_who
-    when FROM_BUYERS
-      by_participant(user, from_who).rated(from_who).average(:buyer_rating)
-      end
-  end
-
-
-
+ 
 
 
 
