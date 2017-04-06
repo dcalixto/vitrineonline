631a4bd849c52ac868eecc66c7d0d84f8347ab30
Daniel Calixto <calixtodaniel@gmail.com>
2017-04-06 00:28:35 -0300

    left product starts to fixaaaaass

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -102,7 +102,7 @@ class ProductsController < ApplicationController
   
   def show
     @product = Product.cached_find(params[:id])
-@user =  @product.probacks.where("users.id = ?", params[:user_id])     
+@user =  @product.probacks.where('user_id IS NOT NULL')     
 canonical_url url_for(@product)
   
 
