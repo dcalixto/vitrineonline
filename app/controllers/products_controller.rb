6e090cf13a06b1131baadf7c2b659748c99a1532
Daniel Calixto <calixtodaniel@gmail.com>
2017-04-07 21:27:32 -0300

    test proback

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -119,8 +119,8 @@ canonical_url url_for(@product)
   # @feedbacks = @q.result(distinct: true).paginate(per_page: 22, page: params[:page])
 
 
-byebug
-  @q = Proback.joins(:product,:user).where('products.id = ? and users.id = ?', @product.id, params[:user_id]).ransack(params[:q])
+#byebug
+  @q = Proback.joins(:product).where('products.id = ?', @product.id).ransack(params[:q])
     
   
 
