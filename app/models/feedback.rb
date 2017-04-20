a3d9e73d66b669d53aef054148fccf20531143e4
Daniel Calixto <calixtodaniel@gmail.com>
2017-04-20 01:07:16 -0300

    order quantitya11aaa1

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -56,7 +56,7 @@ class Feedback < ActiveRecord::Base
 
   def feedback_product
 
-   # order = Order.find_by_id(attributes['order_id'])
+    order = Order.find_by_id(attributes['order_id'])
     feedback = Feedback.find_by_id(attributes['id'])
     order = feedback.order
     product = order.product_id
