a37766ab2df4c2c3d541a8a1488d26d074907db2
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-08 05:20:37 -0300

    removed feedback_counters

--- app/models/pdata.rb
+++ app/models/pdata.rb
@@ -21,8 +21,8 @@ class Pdata < ActiveRecord::Base
 attr_accessible :impressions_count, :color_id, :size_id, :current_step, :is_shared_on_facebook, :is_shared_on_twitter, 
   :cached_votes_total, :cached_votes_score, :cached_votes_up, :cached_votes_down, :cached_weighted_score, 
   
-  :cached_weighted_total, :cached_weighted_average, :buyer_rating, :transaction_id, :buyer_feedback_date, 
-   :rate_from_buyers, :total_feedbacks, :average_rating, :weight, 
+  :cached_weighted_total, :cached_weighted_average,  
+    :total_feedbacks, :average_rating, :weight, 
   :length, :width, :height, :diamenter, :code,
   :id, :vitrine_id, :slug, :name, :price, :detail, :gender_id, :category_id, :subcategory_id, 
   :brand_id, :material_id, :condition_id, :meta_keywords, :quantity, :status,:order_id,:odata_id
