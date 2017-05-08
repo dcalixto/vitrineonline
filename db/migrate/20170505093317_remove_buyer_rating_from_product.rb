870221e0833ccc2e61e1cd0bf44c9c5833775ef4
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-08 02:33:44 -0300

    removed buyer_rating

--- db/migrate/20170505093317_remove_buyer_rating_from_product.rb
+++
@@ -1,9 +0,0 @@
-class RemoveBuyerRatingFromProduct < ActiveRecord::Migration
-  def up
-    remove_column :products, :buyer_rating
-  end
-
-  def down
-    add_column :products, :buyer_rating, :integer
-  end
-end
