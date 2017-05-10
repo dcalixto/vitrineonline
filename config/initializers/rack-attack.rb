468be2d18851139a3a3fddde770d5038aeb91c8a
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-10 19:27:45 -0300

    test

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,42 +1,24 @@
-#class Rack::Attack
-
-  ### Configure Cache ###
+class Rack::Attack
 
 
-  # Throttle all requests by IP (60rpm)
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  #
-  #
-
-  #throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
- #   req.ip # unless req.path.start_with?('/assets')
- # end
-
-  ### Prevent Brute-Force Login Attacks ###
+  throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
+    req.ip # unless req.path.start_with?('/assets')
+  end
 
 
-  # Throttle POST requests to /login by IP address
-  #
-  # Key: "rack::attack:#{Time.now.to_i/:period}:logins/ip:#{req.ip}"
- 
-  
-  #throttle('logins/ip', :limit => 5, :period => 30.seconds) do |req|
-   # if req.path == '/login' && req.post?
-   #   req.ip
-  #  end
- # end
-
-  # Throttle POST requests to /login by email param
-  # on wood!)
-
-  
-  #throttle("logins/email", :limit => 5, :period => 30.seconds) do |req|
-  #  if req.path == '/login' && req.post?
-     #      req.params['email'].presence
-   # end
- # end
-
-#end
+  throttle('logins/ip', :limit => 5, :period => 20.seconds) do |req|
+    if req.path == '/login' && req.post?
+      req.ip
+    end
+  end
 
 
+  throttle("logins/email", :limit => 5, :period => 20.seconds) do |req|
+    if req.path == '/login' && req.post?
+      # return the email if present, nil otherwise
+      req.params['email'].presence
+    end
+  end
+
+
+end
