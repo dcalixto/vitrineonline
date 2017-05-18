e3c56cbfee1c53835ab608c7d830b0349e264f52
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-17 21:06:31 -0300

    hehe

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -7,9 +7,18 @@ class Dispute < ActiveRecord::Base
   belongs_to :order
     belongs_to :seller, foreign_key: 'seller_id', class_name: 'Vitrine'
   belongs_to :buyer, foreign_key: 'buyer_id', class_name: 'User' 
- 
+
   
   
+  
+    attr_accessible :order_id,:seller_id,:buyer_id,:buyer_name,:seller_name,:transaction_id,:status,
+:amount,:motive,:solution,:buyer_comment,:seller_comment,:buyer_file,:seller_file
+
+
+
+
+
+
   STATUSES.each do |method|
     define_method "#{method}?" do
       status == method
