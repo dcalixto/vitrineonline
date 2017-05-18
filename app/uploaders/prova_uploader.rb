172ff372d04791d9091c6f321891505bd4fa7255
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-18 20:01:25 -0300

    sellers

--- app/uploaders/prova_uploader.rb
+++ app/uploaders/prova_uploader.rb
@@ -10,7 +10,7 @@ class ProvaUploader < CarrierWave::Uploader::Base
  include CarrierWave::Video
 
   process encode_video: [:mp4, callbacks: { after_transcode: :set_success } ]
-end
+
 
 
   storage :file
