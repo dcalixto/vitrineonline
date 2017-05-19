142170508b82f951516304c7ec6fa96cff1315d3
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-19 15:18:00 -0300

    hehe

--- app/mailers/dispute_mailer.rb
+++ app/mailers/dispute_mailer.rb
@@ -1,39 +1,39 @@
 class DisputeMailer < ActionMailer::Base
- 
- default from: 'Vitrineonline'
 
- # include Resque::Mailer
-add_template_helper(EmailHelper)
+  default from: 'Vitrineonline'
+
+  # include Resque::Mailer
+  add_template_helper(EmailHelper)
 
 
 
   def dispute_confirmation(dispute)
     @dispute = dispute
-  
+
 
     mail(to: @dispute.buyer_email, subject: 'Reclamação Enviada', &:html)
- 
- # mail(to: @dispute.seller.email, subject: 'Comprador abriu uma Reclamação', &:html)
 
-  
+    # mail(to: @dispute.seller.email, subject: 'Comprador abriu uma Reclamação', &:html)
+
+
   end
 
 
- # def buyer_dispute(dispute)
-  
- #   @dispute = dispute
-  
+  def dispute_update(dispute)
 
- ##   mail(to: @dispute.buyer.email, subject: 'Reclamação Enviada', &:html)
- 
-  
-  
- # end
+    @dispute = dispute
 
-# def seller_dispute(dispute)
-# @dispute = dispute
- #   mail(to: @dispute.seller.email, subject: 'Comprador abriu uma Reclamação', &:html)
- # end
+
+    mail(to: @dispute.buyer_email, subject: 'Reclamação Enviada', &:html)
+
+
+
+  end
+
+  def dispute_finish(dispute)
+    @dispute = dispute
+    mail(to: @dispute.buyer_email, subject: 'Comprador abriu uma Reclamação', &:html)
+  end
 
 
 
