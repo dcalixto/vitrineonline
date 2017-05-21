a7d41794937ed91ce30df0d4a189352454a3e5d5
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-20 22:34:50 -0300

    hehesss

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
 def create
   @dispute = @dispute.find params[:dispute_id]
   @comment = @dispute.comments.new params[:comment]
+  @comment.user = current_user
   if @comment.save
     redirect_to edit_order_dispute_path
 
