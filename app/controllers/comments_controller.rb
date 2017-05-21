652a236277c36ce101bcd46adb62613dc872fb4e
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-21 00:45:36 -0300

    test comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
 
 def create
-  @dispute = @dispute.find params[:dispute_id]
+ # @dispute = @dispute.find params[:dispute_id]
+   @dispute  = Dispute.find_by(id: params[:id])
   @comment = @dispute.comments.new params[:comment]
   @comment.user = current_user
   if @comment.save
