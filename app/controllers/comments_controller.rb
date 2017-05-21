35f492860182c25708d9426ce3b030883b319868
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-21 02:25:40 -0300

    4diass

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,15 @@ def create
  # @dispute = @dispute.find params[:dispute_id]
     @order = Order.find(params[:order_id])
   @dispute = @order.dispute
-  @comment = @dispute.comments.new params[:comment]
-  @comment.user = current_user
+ # @comment = @dispute.comments.new params[:comment]
+  ##@comment.user = current_user
 
 #@comment = @dispute.comments.build(params[:comment])
    # @comment.user = current_user
 
 
-  #@comment =  @dispute.comments.create(:comment => params[:comment], :user => current_user)
+  @comment =  @dispute.comments.create(:comment => params[:comment], :user => current_user)
+@comment.user = current_user
 
   if @comment.save
    redirect_to order_dispute_path
