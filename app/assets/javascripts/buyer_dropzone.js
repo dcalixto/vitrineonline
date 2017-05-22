1c40c13fc4c092db0bdf6d19a81fbb794854336a
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-22 20:18:20 -0300

    testssssss

--- app/assets/javascripts/buyer_dropzone.js
+++ app/assets/javascripts/buyer_dropzone.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     maxFilesize: 2, // Set the maximum file size to 256 MB
     maxFiles: 1,
     dictDefaultMessage: "Solte suas imagens aqui",
-	paramName: "proofs[file]", // Rails expects the file upload to be something like model[field_name]
+	paramName: "proofs[file][]", // Rails expects the file upload to be something like model[field_name]
 	addRemoveLinks: true, // Don't show remove links on dropzone itself.
     dictRemoveFile: 'Remover',
     acceptedFiles: "image/*"
