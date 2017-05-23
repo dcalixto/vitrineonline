4756ebaf241d949ad4e543f86c28f614e9962cf0
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-23 16:12:01 -0300

    o brand fixs

--- db/migrate/20170430025710_create_odata.rb
+++ db/migrate/20170430025710_create_odata.rb
@@ -15,32 +15,29 @@ class CreateOdata < ActiveRecord::Migration
       t.integer :feedback_id
       t.string :track_number
       t.integer  :brand_id                                             
-       t.integer  :obrand_id     
+      t.integer  :obrand_id     
       t.integer  :material_id
       t.integer  :condition_id
       t.integer  :color_id
       t.integer  :size_id
       t.string :seller_name
-       t.string :buyer_name
+      t.string :buyer_name
+      t.integer :order_id
+      t.string :transaction_id
+      t.string :user_address
+      t.string :user_neighborhood
+      t.string :user_city
+      t.string :user_state
+      t.string :user_postal_code
+      ts.tring :vitrine_postal_code
+      t.string :vitrine_address
+      t.string :vitrine_neighborhood
+      t.string :vitrine_city
+      t.string :vitrine_state
+      t.datetime :tcreated_at
+      t.datetime :tupdated_at
 
-
-t.integer :order_id
-
- t.string :transaction_id
-t.string :user_address
-t.string :user_neighborhood
-t.string :user_city
-t.string :user_state
-t.tring :user_postal_code
-ts.tring :vitrine_postal_code
-t.string :vitrine_address
-t.string :vitrine_neighborhood
-t.string :vitrine_city
-t.string :vitrine_state
-t.datetime :tcreated_at
-t.datetime :tupdated_at
-
-t.timestamps
+      t.timestamps
     end
     add_index :odata, :product_id
     add_index :odata, :cart_id
@@ -49,7 +46,7 @@ t.timestamps
     add_index :odata, :feedback_id
 
     add_index :odata, :brand_id
-     add_index :odata, :obrand_id
+    add_index :odata, :obrand_id
 
     add_index :odata, :color_id
     add_index :odata, :size_id
