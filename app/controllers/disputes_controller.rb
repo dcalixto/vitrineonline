4b65acdac48bc4168ebc6e1f676b68630f2a8b2d
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-22 21:22:35 -0300

    testssssssssxz

--- app/controllers/disputes_controller.rb
+++ app/controllers/disputes_controller.rb
@@ -10,9 +10,9 @@ class DisputesController < ApplicationController
     else
       @dispute = Dispute.new
 
-@comment = @dispute.comments.build#(params[:comment])
-    @comment.user = current_user
-   @proof = @dispute.proofs.build
+      @comment = @dispute.comments.build#(params[:comment])
+      @comment.user = current_user
+      @proof = @dispute.proofs.build
     end
   end 
 
@@ -37,13 +37,13 @@ class DisputesController < ApplicationController
 
       dispute.transaction_id = @order.transaction.transaction_id
       dispute.status = !dispute.status
-    
 
-             if dispute.save
 
-#  @comment = @dispute.comments.build(params[:comment])
-   # @comment.user = current_use
-   
+      if dispute.save
+
+        #  @comment = @dispute.comments.build(params[:comment])
+        # @comment.user = current_use
+
 
         redirect_to order_dispute_path(@order, @dispute)
         flash[:success] = 'Reclamação Criada'
@@ -60,14 +60,14 @@ class DisputesController < ApplicationController
   def show
     @dispute = @order.dispute
     @comments = @dispute.comments.all
-    
-@comment = @dispute.comments.build(params[:comment])
-    @comment.user = current_user
-@proof = @dispute.proofs.build(params[:proof])
 
-# params[:images]['file'].each do |a|
- #         @image = @dispute.images.create!(:file => a)
-   #    end
+    @comment = @dispute.comments.build(params[:comment])
+    @comment.user = current_user
+    @proof = @dispute.proofs.build(params[:proof])
+
+    # params[:images]['file'].each do |a|
+    #         @image = @dispute.images.create!(:file => a)
+    #    end
 
 
   end
@@ -83,7 +83,7 @@ class DisputesController < ApplicationController
 
 
     @dispute = @order.dispute
-        dispute.status = !dispute.status
+    dispute.status = !dispute.status
     if dispute.save
 
       DisputeMailer.dispute_finish(@dispute).deliver
@@ -102,7 +102,7 @@ class DisputesController < ApplicationController
 
 
 
- def comment
+  def comment
     dispute = Dispute.find(params[:dispute_id])
     dispute.comments.create(:comment => params[:comment], :user => current_user)
     render :nothing => true
@@ -124,54 +124,54 @@ class DisputesController < ApplicationController
   def update
     @dispute = @order.dispute
 
-  
+
     respond_to do |format|
       format.html do
 
-    if @dispute.update_attributes(params[:dispute])
- #params[:proofs]['file'].each do |a|
-     #     @proof = @dispute.proofs.create!(:file => a)
-     #  end
-      redirect_to order_dispute_path#(@order, @dispute)
-      DisputeMailer.dispute_update(@dispute).deliver
-      flash[:success] = 'Reclamação atualizada'
+        if @dispute.update_attributes(params[:dispute])
+          #params[:proofs]['file'].each do |a|
+          #     @proof = @dispute.proofs.create!(:file => a)
+          #  end
+          redirect_to order_dispute_path#(@order, @dispute)
+          DisputeMailer.dispute_update(@dispute).deliver
+          flash[:success] = 'Reclamação atualizada'
 
-    else
-      render :show
+        else
+          render :show
+        end
+      end
+      format.json do
+
+        if @dispute.update_attributes(params[:dispute])
+          params[:proofs]['file'].each do |a|
+            @proof = @dispute.proofs.create!(:file => a)
+          end
+
+        end
+        render :nothing => true
+
+      end
     end
-    end
-       format.json do
-
-  if @dispute.update_attributes(params[:dispute])
- params[:proofs]['file'].each do |a|
-          @proof = @dispute.proofs.create!(:file => a)
-       end
-
   end
-   render :nothing => true
 
-       end
+
+
+
+
+  def upload
+    @dispute = @order.dispute
+    respond_to do |format|
+      format.json do
+        if @dispute.update_attributes(params[:dispute])
+          params[:proofs]['file'].each do |a|
+            @proof = @dispute.proofs.create!(:file => a)
+          end
+        end
+        render :nothing => true
+      end
+
     end
   end
-
-
-
-
-
-def upload
-
-  if @dispute.update_attributes(params[:dispute])
- params[:proofs]['file'].each do |a|
-          @proof = @dispute.proofs.create!(:file => a)
-       end
-
-
-
-
-end
-end
-
-
 
 
   private
