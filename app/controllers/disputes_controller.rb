b492f093686f51a690da4e795dcca49715702cbe
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-24 20:14:52 -0300

    fix order status boolean again 2

--- app/controllers/disputes_controller.rb
+++ app/controllers/disputes_controller.rb
@@ -38,12 +38,11 @@ class DisputesController < ApplicationController
       dispute.transaction_id = @order.transaction.transaction_id
       #dispute.status = !dispute.status
      
+@order = params[:dispute_status] == "1"
+   @order.update_attributes(:dispute_status => true)
 
       if dispute.save
-    @order = params[:dispute_status] == "1"
-      @order.save
-     # order.update_attributes(:dispute_status => true)
-    
+        
         redirect_to order_dispute_path(@order, @dispute)
         flash[:success] = 'Reclamação Criada'
 
