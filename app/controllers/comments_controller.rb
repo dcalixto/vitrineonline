93f35edfb8101bb8159458c93b5a5476768baa9e
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-26 14:17:57 -0300

    test the comment comment on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,23 @@
 class CommentsController < ApplicationController
 
-#def create
-  
-#  @order = Order.find(params[:order_id])
-#  @dispute = @order.dispute
- # @comment = @dispute.comments.new params[:comment]
-   # @comment = @dispute.comments.create!(params[:comment])
-
- # @comment.user = current_user
-
-
- # if @comment.save
- #  redirect_to order_dispute_path
- # end
-#end
-
-
 def create
-render(text: params[:comment])
+  
+  @order = Order.find(params[:order_id])
+  @dispute = @order.dispute
+  @comment = @dispute.comments.new params[:comment]
+  @comment.comment = params[:comment][:comment]
+
+  #@comment = @dispute.comments.create!(params[:comment])
+
+  @comment.user = current_user
+
+
+  if @comment.save
+   redirect_to order_dispute_path
+  end
 end
+
+
 
 
 
