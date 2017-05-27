8af1d103c5a685356e6a18d3de20b91e43a11cfe
Daniel Calixto <calixtodaniel@gmail.com>
2017-05-27 04:00:49 -0300

    pass paypal to live

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,22 +1,22 @@
-class Rack::Attack
+#class Rack::Attack
 
 
-  throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
-    req.ip   end
+ # throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
+ #   req.ip   end
 
 
- throttle('logins/ip', :limit => 5, :period => 20.seconds) do |req|
-  if req.path == '/login' && req.post?
-      req.ip
-   end
-  end
+# throttle('logins/ip', :limit => 5, :period => 20.seconds) do |req|
+#  if req.path == '/login' && req.post?
+#      req.ip
+#   end
+#  end
 
 
-  throttle("logins/email", :limit => 5, :period => 20.seconds) do |req|
-    if req.path == '/login' && req.post?
-         req.params['email'].presence
-    end
-  end
+#  throttle("logins/email", :limit => 5, :period => 20.seconds) do |req|
+#    if req.path == '/login' && req.post?
+ #        req.params['email'].presence
+#    end
+#  end
 
 
-end
+#end
